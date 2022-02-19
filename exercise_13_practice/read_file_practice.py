12c835f6d768362f2838301784eb2aac0108ce92
Jody Broad <msjodybroad@gmail.com>
2022-02-19 17:21:59 +0000

    minor revisions

--- exercise_13_practice/read_file_practice.py
+++ exercise_13_practice/read_file_practice.py
@@ -3,20 +3,21 @@ infile = open('pelican.txt', 'r')
 # checks the file type and assigns to the variable filetype
 filetype = type(infile)
 # _io.TextIOWrapper is the file type
-print(filetype)
+print("File type is : ", filetype, "\n")
 # this is not printing the contents of the file, just its metadata
 # print(infile)
 # .read will read and print the entire thing in one go
-print(infile.read())
+print("If we print the whole thing in one go: \n \n", infile.read())
 
 # create a list of the individual lines
 limerick_list = open('pelican.txt').readlines()
-print(limerick_list)
+print("Printing the limerick as a list: \n", limerick_list, "\n")
 # calculate how many lines there are
 limerick_length = len(limerick_list)
-print(limerick_length)
+print("The length of the limerick is: ", limerick_length, "lines \n ")
 
 # a loop to print line by line using the file object iterator, end="" removes blank lines
+print("Using a loop we can print line by line: \n")
 for line in open('pelican.txt'):
     print(line, end="")
 
